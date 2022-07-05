dd4c84be23bd8bc50e77a7f64f0fc4e4652c209c
tonyxuan2021 <tonyyexuan@gmail.com>
2022-07-05 15:09:41 -0700

    create route branch

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Cart from './pages/Cart';
-import Header from './components/Header';
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Cart from "./pages/Cart";
+import Header from "./components/Header";
+import Language from "./components/Language";
+import SignIn from "./components/SignIn";
+import SignUp from "./components/SignUp";
+import Deal from "./components/Deal";
+import CustomerService from "./components/CustomerService";
+import Registry from "./components/Registry";
+import GiftCard from "./components/GiftCard";
+import Sell from "./components/Sell";
 
 function App() {
   return (
@@ -11,9 +18,17 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/cart" element={<Cart />} />
+        <Route path="/language" element={<Language />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/deal" element={<Deal />} />
+        <Route path="/customer" element={<CustomerService />} />
+        <Route path="/registry" element={<Registry />} />
+        <Route path="/giftfcard" element={<GiftCard />} />
+        <Route path="/sell" element={<Sell />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
