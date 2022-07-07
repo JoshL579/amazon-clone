4b0f684240d043c982aebe502278e1b351caad5c
tonyxuan2021 <tonyyexuan@gmail.com>
2022-07-07 12:10:36 -0700

    complete category component styling

---
+++ src/components/Home/Categories.js
@@ -0,0 +1,33 @@
+import { Grid, Paper, Typography } from "@mui/material";
+import React from "react";
+import { Link } from "react-router-dom";
+import categories from "../../data/data.json";
+
+const styles = {
+  p: {
+    padding: 2,
+  },
+};
+
+export default function Categories() {
+  return (
+    <Grid container spacing={3} sx={styles.p}>
+      {categories.map((category) => {
+        const { image, note, title } = category;
+        return (
+          <Grid item xs={3}>
+            <Paper>
+              <Typography variant="h6" fontWeight={700}>{title}</Typography>
+              <Link to="/">
+                <img src={image} className="img" />
+              </Link>
+              <Typography>
+                <Link to="/">{note}</Link>
+              </Typography>
+            </Paper>
+          </Grid>
+        );
+      })}
+    </Grid>
+  );
+}
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,12 +1,17 @@
-import React from 'react';
-import Header from '../../components/Header';
+import { Grid, Paper, Typography } from "@mui/material";
+import React from "react";
+import Header from "../../components/Header";
+import Categories from "../../components/Home/Categories";
+import categories from "../../data/data.json";
+
+const styles = {
+    p: {
+        padding: 2
+    }
+}
 
 export default function Home() {
-    
-
-    return (
-        <div>
-            Home
-        </div>
-    )
+  return (
+    <Categories />
+  );
 }
