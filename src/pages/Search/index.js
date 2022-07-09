9a9c6752c174eeff55ebd93c16301c955870956c
tonyxuan2021 <tonyyexuan@gmail.com>
2022-07-09 16:31:50 -0700

    start search page

--- src/pages/Search/index.js
+++ src/pages/Search/index.js
@@ -1,7 +1,59 @@
+import {
+  FormControl,
+  MenuItem,
+  Grid,
+  Paper,
+  Select,
+  Typography,
+} from "@mui/material";
 import React from "react";
 
+const styles = {
+  flex: {
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  width: {
+    width: "100%",
+  },
+};
+
 const Search = () => {
-  return <div>index</div>;
+  const [age, setAge] = React.useState("");
+
+  const handleChange = (event) => {
+    setAge(event.target.value);
+  };
+
+  return (
+    <Grid container>
+      <Grid item sx={styles.width}>
+        <Paper sx={styles.flex} elevation={2}>
+          <Typography>
+            1-16 of 123 results for "iphone external battery
+          </Typography>
+          <FormControl sx={{ minWidth: 120 }}>
+            <Select
+              displayEmpty
+              onChange={handleChange}
+              value={age}
+              inputProps={{ "aria-label": "Without label" }}
+              sx={{ borderRadius: 15 }}
+            >
+              <MenuItem value="">
+                <em>Sort by: Featured</em>
+              </MenuItem>
+              <MenuItem value={10}>Sort by:Price: Low to High</MenuItem>
+              <MenuItem value={10}>Sort by:Price: High to Low</MenuItem>
+              <MenuItem value={10}>Sort by:Avg. Customer Review</MenuItem>
+              <MenuItem value={30}>Newest Arrivals</MenuItem>
+            </Select>
+          </FormControl>
+        </Paper>
+      </Grid>
+    </Grid>
+  );
 };
 
 export default Search;
