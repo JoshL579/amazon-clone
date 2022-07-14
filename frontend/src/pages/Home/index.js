39af172be879db359d26c5dadf414381ef1cdb97
tonyxuan2021 <tonyyexuan@gmail.com>
2022-07-14 13:47:41 -0700

    homepage data fetch WIP, pc and toy section complete

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -1,59 +1,62 @@
-import { Box, Grid, Paper, Typography } from '@mui/material'
-import React, { useState, useEffect } from 'react'
-import Categories from './components/Categories'
-import Electronics from './components/Electronics'
-import TopSlider from './components/TopSlider'
-import Game from './components/Game'
-import History from './components/History'
-import SignInBtn from './components/SignInBtn'
-import { HomeSwiper } from './components/HomeSwiper'
-import { getImages } from '../../api/endpoints/home'
+import { Box, Grid, Paper, Typography } from "@mui/material";
+import React, { useState, useEffect } from "react";
+import Categories from "./components/Categories";
+import Electronics from "./components/Electronics";
+import TopSlider from "./components/TopSlider";
+import Game from "./components/Game";
+import History from "./components/History";
+import SignInBtn from "./components/SignInBtn";
+import { HomeSwiper } from "./components/HomeSwiper";
+import { getImages } from "../../api/endpoints/home";
 
 const styles = {
   root: {
-    background: '#EAEDED',
+    background: "#EAEDED",
   },
   container: {
     maxWidth: 1500,
-    margin: 'auto',
+    margin: "auto",
   },
-}
+};
 
 export default function Home() {
   const [loading, setLoading] = useState(true);
   const [images, setImages] = useState({});
   useEffect(() => {
-    getImages().then((res) => {
-      setImages(res)
-      setLoading(false)
-    }).catch(err => {
-      setLoading(false)
-    })
-  }, [])
+    getImages()
+      .then((res) => {
+        setImages(res);
+        setLoading(false);
+      })
+      .catch((err) => {
+        setLoading(false);
+      });
+  }, []);
 
-  if (loading) return <></>
+  if (loading) return <></>;
 
   return (
     <Grid container sx={styles.root}>
       <Grid item container sx={styles.container}>
         <TopSlider images={images.heros} />
         <Categories images={images.cards.filter((_, i) => i < 8)} />
-        <HomeSwiper type="Home"
-          images={
-            images.products.filter(
-              (product) => product.categoryId === 7
-            )
-          }
+        <HomeSwiper
+          type="Home"
+          images={images.products.filter((product) => product.categoryId === 7)}
         />
         <Categories images={images.cards.filter((_, i) => i >= 8 && i < 12)} />
-        <HomeSwiper type="Book"
-          images={
-            images.products.filter(
-              (product) => product.categoryId === 5
-            )
-          }
+        <HomeSwiper
+          type="Book"
+          images={images.products.filter((product) => product.categoryId === 5)}
         />
-        {/* <HomeSwiper type="book" /> */}
+        <HomeSwiper
+          type="Toy"
+          images={images.products.filter((product) => product.categoryId === 1)}
+        />
+        <HomeSwiper
+          type="PC"
+          images={images.products.filter((product) => product.categoryId === 3)}
+        />
         {/* <Electronics /> */}
         {/* <HomeSwiper type="home"/>
         <HomeSwiper type="kitchen"/> */}
@@ -65,5 +68,5 @@ export default function Home() {
         <SignInBtn />
       </Grid>
     </Grid>
-  )
+  );
 }
