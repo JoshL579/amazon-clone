b429ce0f4c419843eb782df329cb4b00d23264dc
tonyxuan2021 <tonyyexuan@gmail.com>
2022-07-14 13:42:05 -0700

    save before prisma migrate to prevent error

--- backend/node/mapper/products.js
+++ backend/node/mapper/products.js
@@ -1,27 +1,43 @@
-const { PrismaClient } = require('@prisma/client')
+const { PrismaClient } = require("@prisma/client");
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
 const findAllProducts = (categories, limit) => {
-    return prisma.products
-        .findMany({
-            take: limit,
-            where: {
-                categoryId: {
-                    in: categories
-                }
-            }
-        })
-        .then((products) => {
-            return products
-        })
-        .catch((err) => {
-            return null
-        })
-}
+  return prisma.products
+    .findMany({
+      take: limit,
+      where: {
+        categoryId: {
+          in: categories,
+        },
+      },
+    })
+    .then((products) => {
+      return products;
+    })
+    .catch((err) => {
+      return null;
+    });
+};
+
+const fingSingleProduct = (id) => {
+  return prisma.products
+    .findUnique({
+      where: {
+        id: id,
+      },
+    })
+    .then((product) => {
+      return product;
+    })
+    .catch((err) => {
+      return null;
+    });
+};
 
 const productDbHandler = {
-    findAllProducts: findAllProducts,
-}
+  findAllProducts: findAllProducts,
+  fingSingleProduct: fingSingleProduct,
+};
 
-module.exports = productDbHandler
+module.exports = productDbHandler;
