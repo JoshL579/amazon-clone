b56558a0398dc8c05c7fe3af977bb0277ed61b21
Josh Liu <bvup66@gmail.com>
2022-11-06 22:56:52 -0800

    fix view engine

--- backend/node/app.js
+++ backend/node/app.js
@@ -13,14 +13,14 @@ const productsRouter = require('./routes/products')
 const app = express()
 
 // view engine setup
-// app.set('views', path.join(__dirname, 'views'))
-// app.set('view engine', 'jade')
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'jade')
 
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
-// app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')))
 app.use(cors())
 
 app.use('/', indexRouter)
